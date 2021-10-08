5f712f64ea1e578f87d1ae27ebf96bcc2115ac29
tamaken0802 <85724739+tamaken0802@users.noreply.github.com>
2021-10-08 15:30:47 +0900

    a

---
+++ sample/js/script01.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $("#button01").on("click",function(){
+        $(".back-red").slideUp();
+    })
+
+    $("#button02").on("click",function(){
+        $(".back-blue").slideUp();
+    })
+
+    $("#button03").on("click",function(){
+        $("3box03,#box07,#box08").slideUp();
+    })
+
+    $("#button04").on("click",function(){
+        $(".box").show();
+    })
+
+})
